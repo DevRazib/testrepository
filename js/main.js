7b2832a10ab22d25aaa77c30ae1545e94a57142f
Your Name <ahmedrazib477@gmail.com>
2021-07-24 16:20:05 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//banner Owl-carousel
+$(".owl-carousel").owlCarousel({
+    margin: 30,
+    loop: true,
+    autoplay: true,
+    center: true,
+    items: 1,
+    dots: true
+});
+/* Counter Up */
+$('.counter-start').counterUp({
+    delay: 10,
+    time: 1000
+});
